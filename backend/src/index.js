265ed0d6dc0b7cc4b392522b18bac39af9797f94
Chaitanya Sahu <chaitanya.sahu666@gmail.com>
2025-05-20 02:17:26 +0530

    Fixed bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 const PORT=process.env.PORT;
 
 if(process.env.NODE_ENV==='production'){
-  app.use(permission.static(path.join(__dirname,'../frontend/dist')));
+  app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
