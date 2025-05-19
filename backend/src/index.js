3aa90cc1224a1b60338f6c94fc77802e442fd552
Chaitanya Sahu <chaitanya.sahu666@gmail.com>
2025-05-20 02:51:05 +0530

    fixed production bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,13 +35,13 @@ app.get('/', (req, res) => {
 
 const PORT=process.env.PORT;
 
-if(process.env.NODE_ENV==='production'){
+// if(process.env.NODE_ENV==='production'){
   app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
   })
-}
+// }
 
 server.listen(PORT, () => {
     console.log('Server is running on port',PORT);
